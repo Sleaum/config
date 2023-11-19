fa688fc136f9c6f0c854825646b4a4d54a9d84bf
Sleaum <sleaum31@gmail.com>
2023-11-19 13:03:06 +0100

    Update NAS.sh

--- NAS.sh
+++ NAS.sh
@@ -1,2 +1,6 @@
 #sudo apt install nfs-common
 sudo mount -t nfs -o rw 192.168.1.37:/c/media /home/nut/NAS/
+
+
+#https://192.168.1.37/admin (netgear1) tls 1.2 error
+#https://www.linuxcapable.com/install-pale-moon-on-ubuntu-linux/
